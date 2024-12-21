0c186e6b1e89ea894e03a0f77a0032ea18a3c29a
DarmilaT <darmila02.t@gmail.com>
2024-12-21 09:28:41 +0530

    feat: evaluate the spell checker

--- src/spell_checker/run_spell_checker.py
+++ src/spell_checker/run_spell_checker.py
@@ -1,12 +1,21 @@
 from spell_checker import SpellChecker
+from tests.spell_checker_evaluator import evaluate_spell_checker
 
 correct_words_file = r"D:\7th Semester\AI\Tamil-SpellGrammar-Checker\data\processed\extracted_words.txt"
 
-user_input = "நடைபெறற"
+test_paragrapgh = "ஏற்றுமதயை நம்பியுள்ள நாடுகளுகுத்தான் இதனால் பாதிப்பு. உள்நாட்டிலேயே தேவை அதிகமக உள்ள இந்தியா போன்ற மக்கள் தொகை அதிகம் கொண்ட வளரும் பொருளதர நாட்டில் பிரச்சனை ஏற்படது என்று அவர் திடவட்டமாகக் கூறினார்."
+expected_paragrapgh = "ஏற்றுமதியை நம்பியுள்ள நாடுகளுக்குத்தான் இதனால் பாதிப்பு. உள்நாட்டிலேயே தேவை அதிகமாக உள்ள இந்தியா போன்ற மக்கள் தொகை அதிகம் கொண்ட வளரும் பொருளாதார நாட்டில் பிரச்சனை ஏற்படாது என்று அவர் திட்டவட்டமாகக் கூறினார்."
 
-spell_checker = SpellChecker(correct_words_file, user_input)
+test_sentences = test_paragrapgh.rstrip(".").split(".")
+expected_sentences = expected_paragrapgh.rstrip(".").split(".")
 
-corrected_text = spell_checker.correct()
+for i, sentence in enumerate(test_sentences):
+    print(f"\n\nTest Sentence: {sentence}")
+    spell_checker = SpellChecker(correct_words_file, sentence)
+    corrected_text = spell_checker.correct()
 
-# Output the corrected text
-print(f"Corrected Text: {corrected_text}")
+    # Output the corrected text
+    print(f"Corrected Text: {corrected_text}\n")
+
+    # Evaluate the spell checker
+    evaluate_spell_checker(corrected_text, expected_sentences[i], corrected_text)
